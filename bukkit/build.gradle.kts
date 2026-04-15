c671022d995575a8846eec07881db1459f477110
lightPlugins <philipp.business@outlook.de>
2026-04-15 20:02:57 +0200

    Update build configuration to use new Paper API and adjust dependencies

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -1,27 +1,26 @@
 plugins {
     id("java-library")
     id("maven-publish")
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
 }
 
 group = "io.nexstudios.menuservice"
 version = providers.gradleProperty("serviceVersion").get()
 
 dependencies {
-    api(project(":common"))
 
-    paperweight.paperDevBundle(providers.gradleProperty("paperVersion").get())
-    compileOnly("net.kyori:adventure-text-serializer-ansi:4.26.1")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT") {
+        exclude(group = "org.apache.commons", module = "commons-lang3")
+        exclude(group = "org.codehaus.plexus", module = "plexus-utils")
+    }
+    api("com.github.lightplugins:NexServiceRegistry:${providers.gradleProperty("registryVersion").get()}")
 
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks {
-    assemble {
-        dependsOn(reobfJar)
-    }
+tasks.test {
+    useJUnitPlatform()
 }
 
 publishing {
